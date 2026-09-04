448a1ca37b926fb1cc69f25ff880006aac8bab8c
Walmann <tovare@walmann.it>
2026-09-04 11:39:43 +0200

    Fixed node installation.

--- common/pipInstall.py
+++ common/pipInstall.py
@@ -3,6 +3,9 @@ import subprocess
 import sys
 from pathlib import Path
 
+from log import log
+
+
 def pipInstall(modules:list):
     for e in modules:
         subprocess.check_call([sys.executable, "-m", "pip", "install", e])
@@ -10,8 +13,10 @@ def pipInstall(modules:list):
 def pipInstall_file(req_file:Path):
     reqs = []
 
-    with open(req_file, "r") as file:
-        for line in file.readlines():
-            reqs.append(line)
-    pipInstall(reqs)
-    
+    try:
+        with open(req_file, "r") as file:
+            for line in file.readlines():
+                reqs.append(line)
+        pipInstall(reqs)
+    except FileNotFoundError:
+        log("No requirements.txt file found. Skipping.")
