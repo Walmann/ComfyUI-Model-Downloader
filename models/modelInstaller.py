41a8ff9373bacfaf8086b7d87f20073096b3b33b
Walmann <tovare@walmann.it>
2026-09-04 12:41:08 +0200

    Made modelinstaller easier to use

--- models/modelInstaller.py
+++ models/modelInstaller.py
@@ -1,6 +1,6 @@
 import os
 import configparser
-
+import sys
 
 from nodes import Install_nodes, get_repo_section
 from common import log
@@ -12,18 +12,27 @@ from settings import config
 def modelInstaller(modelList: list, settings: configparser.ConfigParser, dryRun=False):
     setting = config()
 
-    
-    if "minimaxh3" in modelList:
-        log("Installing MiniMaxH3")
-        log("SKIPPING: NODES NOT IMPLEMENTED YET!", "WARNING")
-        nodes = get_repo_section("MiniMaxH3")
-        Install_nodes(settings=setting, nodes=nodes)
+    for m in modelList:
+        try:
+            try:      
+                log(f"Installing {m}")
+                nodes = get_repo_section(m)
+                Install_nodes(settings=setting, nodes=nodes)
+            except KeyError as e: 
+                log(f"Could not find {m} in node database! This is fine.")
 
-        log("Downloading MiniMaxH3 models")
-        huggingface.hugginface_downloadModel(model="MiniMaxH3", settings=setting, dryRun=dryRun)
-        pass
-
+            try:
+                log("Downloading MiniMaxH3 models")
+                huggingface.hugginface_downloadModel(model=m, settings=setting, dryRun=dryRun)
+                pass
+            except KeyError as e: 
+                log(f"Could not find {m} in model database!", "ERROR")
+                sys.exit()
+        except Exception as e:
+            log(f"ERROR INSTALLING MODEL ${m}", "ERROR")
+            log(e)
+            sys.exit()
 
 if __name__ == "__main__":
-    modelInstaller(modelList=["minimaxh3"], settings=config(isDebug=True), dryRun=True)
+    modelInstaller(modelList=["MiniMaxH3", "Krea2"], settings=config(isDebug=True), dryRun=True)
     pass
